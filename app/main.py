991e7faca74332165e73446b8ae8695dc2e1ae3e
zubairkofficial <mzubairkhan.official@gmail.com>
2024-08-12 20:21:14 +0500

    first authentication

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ from app.models.User import User
 from app.models.PasswordReset import PasswordReset
 from app.routes.user_route import router as user_router
 import logging
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 
 
@@ -23,6 +26,15 @@ async def lifespan(app= FastAPI):
         
 app = FastAPI(lifespan=lifespan)
 app.include_router(user_router, prefix='/auth', tags=['User'])
+
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def index():
